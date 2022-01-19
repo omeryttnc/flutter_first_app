a74168c0ea65c42925f188ec9f1d68cf0de98605
omeryttnc <67800186+omeryttnc@users.noreply.github.com>
2022-01-19 10:36:35 +0000

    19.01.2022

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(const MaterialApp(
-      debugShowCheckedModeBanner: false,
+void main() =>
+    runApp(const MaterialApp(
       home: Home(),
     ));
 
@@ -16,32 +16,38 @@ class Home extends StatelessWidget {
         centerTitle: true,
         backgroundColor: Colors.red[600],
       ),
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-        crossAxisAlignment: CrossAxisAlignment.center,
+      body: Row(
+        //crossAxisAlignment: CrossAxisAlignment.center,
+        //mainAxisAlignment: MainAxisAlignment.spaceAround,
         children: [
-          Container(
-            padding: const EdgeInsets.all(10.0),
-            color: Colors.green,
-            child: const Text('one'),
+
+          Expanded(
+            flex: 1,
+            child: Container(
+              padding: EdgeInsets.all(20.0),
+              child: Text('one'),
+              color: Colors.blue,
+            ),
           ),
-          Container(
-            padding: const EdgeInsets.all(20.0),
-            color: Colors.grey,
-            child: const Text('one'),
-          ),
-          Container(
-            padding: const EdgeInsets.all(30.0),
+          Expanded(
+            flex: 2,
+            child: Container(
+              padding: EdgeInsets.all(20.0),
+              child: Text('one'),
+              color: Colors.green,
+            ),
+          ), Container(
+            padding: EdgeInsets.all(20.0),
+            child: Text('one'),
             color: Colors.red,
-            child: const Text('one'),
           ),
-          Container(
-            padding: const EdgeInsets.all(40.0),
-            color: Colors.cyan,
-            child: const Text('one'),
-          ),
+
+         Expanded(
+             flex: 2,
+             child: Image.asset('assets/pictures/picture-1.jpg')),
         ],
       ),
+
 
       floatingActionButton: FloatingActionButton(
         onPressed: () {},
