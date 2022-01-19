55c345335fe5379e2822608a6bd3f6bc2bf62082
omeryttnc <67800186+omeryttnc@users.noreply.github.com>
2022-01-19 09:21:20 +0000

    first

---
+++ lib/main.dart
@@ -0,0 +1,55 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(const MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Home(),
+    ));
+
+class Home extends StatelessWidget {
+  const Home({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('my first app'),
+        centerTitle: true,
+        backgroundColor: Colors.red[600],
+      ),
+      body: Column(
+        children: [
+          Container(
+            padding: const EdgeInsets.all(10.0),
+            color: Colors.green,
+            child: const Text('one'),
+          ),
+          Container(
+            padding: const EdgeInsets.all(20.0),
+            color: Colors.grey,
+            child: const Text('one'),
+          ),
+          Container(
+            padding: const EdgeInsets.all(30.0),
+            color: Colors.red,
+            child: const Text('one'),
+          ),
+          Container(
+            padding: const EdgeInsets.all(40.0),
+            color: Colors.cyan,
+            child: const Text('one'),
+          ),
+        ],
+      ),
+
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {},
+        child: const Center(
+          child: Text(
+            'click',
+            style: TextStyle(fontSize: 10),
+          ),
+        ),
+      ),
+    );
+  }
+}
